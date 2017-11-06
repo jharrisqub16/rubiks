54e285cffc814259ca297578f814a20b853cee11
dball03 <dball03@qub.ac.uk>
2017-11-06 12:56:47 +0000

    Add script to aid motor debugging

---
+++ scripts/motorControl.py
@@ -0,0 +1,28 @@
+################################################################################
+# Script to prompt user to input move to pass on to the arduino.
+# Can be used to debug motor issues
+################################################################################
+import serial
+
+from random import randint
+
+########################################
+## MISC VARIABLES
+########################################
+
+validMoves={"U","F","D","R","L","B","U'","F'","D'","R'","L'","B'","U2","F2","D2","R2","L2","B2"}
+
+def main():
+    print("Enter cube move:")
+
+    arduino = serial.Serial('/dev/ttyACM0', 9600)
+
+    while (1):
+
+        userInput = str(raw_input())
+        if userInput in validMoves: 
+            arduino.write(userInput)
+        else:
+            print("Not a valid move")
+
+main()
