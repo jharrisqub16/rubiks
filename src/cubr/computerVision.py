19bd6045c80d7768b8cdb41acbe8960846b6210b
dball03 <dball03@qub.ac.uk>
2018-01-22 22:40:24 +0000

    ComputerVision: Inital rework of colour recognition: NOT WORKING
    TODO:
    The contours should now be worked out iteratively (althought not
    testing, probably not working).
    However, the relationship between the face colours (the colour of the
    centres), and the face notation needs to be worked out AFTERWARDS.
    Then the contour list can be translated into the final cubestate list

--- src/cubr/computerVision.py
+++ src/cubr/computerVision.py
@@ -4,6 +4,8 @@ import numpy as np
 
 from correlation import *
 
+from operator import add,sub
+
 
 class computerVision():
     def __init__(self):
@@ -46,18 +48,15 @@ class computerVision():
         # TODO this must be modified: HSV values will be derived from
         # calibration, plus more complex algorithm (max liklihood) used to
         # determine best matches.
-        self.lower_yellow = np.array([26,70,100], dtype=np.uint8)
-        self.upper_yellow = np.array([36,255,255], dtype=np.uint8)
-        self.lower_blue = np.array([101,110,100], dtype=np.uint8)
-        self.upper_blue = np.array([150,255,255], dtype=np.uint8)
-        self.lower_orange = np.array([10,140,100], dtype=np.uint8)
-        self.upper_orange = np.array([24,255,255], dtype=np.uint8)
-        self.lower_green = np.array([37,80,100], dtype=np.uint8)
-        self.upper_green = np.array([100,255,255], dtype=np.uint8)
-        self.lower_red1 = np.array([0,80,100], dtype=np.uint8)
-        self.upper_red1 = np.array([7,255,255], dtype=np.uint8)
-        self.lower_red2 = np.array([160,80,100], dtype=np.uint8)
-        self.upper_red2 = np.array([179,255,255], dtype=np.uint8)
+        self.colourCorrelation= {    'Y': (np.array([31,70,100], dtype = np.uint8)),
+                                'B': (np.array([125,110,100], dtype = np.uint8)),
+                                'O': (np.array([18,140,100], dtype = np.uint8)),
+                                'G': (np.array([70,80,100], dtype = np.uint8)),
+                                'R': (np.array([0,80,100], dtype = np.uint8))}
+                                #'R': (np.array([0,80,100], dtype = np.uint8)),
+                                #'W': (np.array([8,8,8], dtype = np.uint8))}
+
+        self.colourOffset = np.array([10,10,10], dtype = np.uint8)
 
         # Defines which camera's image will be output to the GUI:
         # This is done to confine of the number of cameras, capture objects etc
@@ -304,121 +303,44 @@ class computerVision():
 
 
     def extractColours(self, image, cameraNum):
-    # TODO this needs reworked a lot
-    #YELLOW cube detection
-        yellowHSVMask = self.getColourMask(image ,self.lower_yellow, self.upper_yellow)
+        for colourValueCorrelation in colourCorrelation:
+            print(colour)
 
-        yellowContours, hierarchy_y = cv2.findContours(yellowHSVMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        # TODO Is this correct?
-        #cnts = sorted(yellowContours, key = cv2.contourArea, reverse = True)[:25]
-        cnts = sorted(yellowContours, key = cv2.contourArea)[:25]
+            tempLowerLimit = map(sub, self.colourValueCorrelation[element], self.colourOffset)
+            tempUpperLimit = map(add, self.colourValueCorrelation[element], self.colourOffset)
+            # Set S and V upper to max values (255)
+            tempUpperLimit[1] = 255
+            tempUpperLimit[2] = 255
 
-        # TODO Is this correct?
-        for c in cnts:
-            M = cv2.moments(c)
-            area = cv2.contourArea(c)
+            tempMask = self.getColourMask(image, tempLowerLimit, tempUpperLimit)
 
-            #TODO Why is the decision amde this way?
-            if M["m00"] != 0 and area > self.minimumContourArea:
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-            else:
-                cX, cY = 0, 0
+            contours, hierarchy = cv2.findContours(tempMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-            listPosition = self.correlateCubePosition(cameraNum, cX, cY)
-            if (listPosition is not None):
-                self.contourList[listPosition] = [c, cameraNum, (0,255,255)]
-                self.listifyCubePosition(listPosition, 3)
+            # Sort contours such that insertion of largest contours will occur last
+            cnts = sorted(contours, key = cv2.contourArea)[:25]
 
-    #BLUE cube detection
-        blueHSVMask = self.getColourMask(image ,self.lower_blue, self.upper_blue)
+            for c in cnts:
+                M = cv2.moments(c)
+                area = cv2.contourArea(c)
 
-        blueContours, hierarchy_b = cv2.findContours(blueHSVMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        #cnts = sorted(blueContours, key = cv2.contourArea, reverse = True)[:25]
-        cnts = sorted(blueContours, key = cv2.contourArea)[:25]
+                if M["m00"] != 0 and area > self.minimumContourArea:
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+                # TODO why is this else needed?
+                else:
+                    cX, cY = 0, 0
 
-        for c in cnts:
-            M = cv2.moments(c)
-            area = cv2.contourArea(c)
+                listPosition = self.correlateCubePosition(cameraNum, cX, cY)
+                if (listPosition is not None):
+                    # Insert this contour into the contour list
+                    # TODO add area check, disallow camera to overwrite larger contours from a previous camera
+                    # TODO add method to determine the colour that should be associated with this contour
+                    self.contourList[listPosition] = [c, cameraNum, (0,255,255)]
 
-            if M["m00"] != 0 and area > self.minimumContourArea:
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-            else:
-                cX, cY = 0, 0
+                    # Insert into the final cube list
+                    # TODO The colour cannot be assumed yet: Need to work out the relationship between faces and colour first.
+                    self.listifyCubePosition(listPosition, 3)
 
-            listPosition = self.correlateCubePosition(cameraNum, cX, cY)
-            if (listPosition is not None):
-                self.contourList[listPosition] = [c, cameraNum, (255,0,0)]
-                self.listifyCubePosition(listPosition, 1)
-
-    #ORANGE cube detection
-        orangeHSVMask = self.getColourMask(image, self.lower_orange, self.upper_orange)
-        orangeContours, hierarchy_o = cv2.findContours(orangeHSVMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        #cnts = sorted(orangeContours, key = cv2.contourArea, reverse = True)[:25]
-        cnts = sorted(orangeContours, key = cv2.contourArea)[:25]
-
-        for c in cnts:
-            M = cv2.moments(c)
-            area = cv2.contourArea(c)
-
-            if M["m00"] != 0 and area > self.minimumContourArea:
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-            else:
-                cX, cY = 0, 0
-
-            listPosition = self.correlateCubePosition(cameraNum, cX, cY)
-            if (listPosition is not None):
-                self.contourList[listPosition] = [c, cameraNum, (0,160,255)]
-                self.listifyCubePosition(listPosition, 5)
-
-    #GREEN cube detection
-        greenHSVMask = self.getColourMask(image, self.lower_green, self.upper_green)
-        greenContours, hierarchy_g = cv2.findContours(greenHSVMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        #cnts = sorted(greenContours, key = cv2.contourArea, reverse = True)[:25]
-        cnts = sorted(greenContours, key = cv2.contourArea)[:25]
-
-        for c in cnts:
-            M = cv2.moments(c)
-            area = cv2.contourArea(c)
-
-            if M["m00"] != 0 and area > self.minimumContourArea:
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-            else:
-                cX, cY = 0, 0
-
-            listPosition = self.correlateCubePosition(cameraNum, cX, cY)
-            if (listPosition is not None):
-                self.contourList[listPosition] = [c, cameraNum, (0,255,0)]
-                self.listifyCubePosition(listPosition, 4)
-
-    #RED cube detection
-        redHSVMask1 = self.getColourMask(image, self.lower_red1, self.upper_red1)
-        redHSVMask2 = self.getColourMask(image, self.lower_red2, self.upper_red2)
-        redHSVMask = cv2.bitwise_or(redHSVMask1, redHSVMask2)
-
-        redContours , hierarchy_r = cv2.findContours(redHSVMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        #cnts = sorted(redContours, key = cv2.contourArea, reverse = True)[:25]
-        cnts = sorted(redContours, key = cv2.contourArea)[:25]
-
-        for c in cnts:
-            M = cv2.moments(c)
-            area = cv2.contourArea(c)
-
-            if M["m00"] != 0 and area > self.minimumContourArea:
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-            else:
-                cX, cY = 0, 0
-
-            listPosition = self.correlateCubePosition(cameraNum, cX, cY)
-            if (listPosition is not None):
-                self.contourList[listPosition] = [c, cameraNum, (0,0,255)]
-                self.listifyCubePosition(listPosition, 2)
 
     def getColourMask(self, colouredImage, lowerThreshold, upperThreshold):
         hsvImage = cv2.cvtColor(colouredImage, cv2.COLOR_BGR2HSV)
