35d02a597f38c0f0cbe0bc296f0e065be9a42d4d
dball03 <dball03@qub.ac.uk>
2018-03-14 11:10:23 +0000

    cuber.py: Remove todo and update comments

--- src/cuber/cuber.py
+++ src/cuber/cuber.py
@@ -36,8 +36,7 @@ class Cuber():
                             'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L',
                             'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B')
 
-        #TODO not sure where this should originate: for second iteration,
-        # probably needs to be derived by CV, and passed on as required.
+        # This is just a static list of faces, used to generate scrambles.
         self.colours = {0: 'U', 1: 'R', 2: 'F', 3: 'D', 4: 'L', 5: 'B'}
 
 
@@ -99,13 +98,16 @@ class Cuber():
         lastOperation = None
 
         for x in range(0, moves):
-            operation = randint(0,5)
+            # Generate the face to turn (based on index of colours dict)
+            # Also, disallow the same face from being turned consecutively
+            operation = randint(0,len(self.colours))
             while (operation == lastOperation):
-                operation = randint(0,5)
+                operation = randint(0,len(self.colours))
 
+            # Update the last operation
             lastOperation = operation
 
-            # Perform half turn (2X) if 1
+            # Generate 3 cases of rotation direction (standard, ' and 2)
             rotation = randint(0,2)
 
             if rotation == 0 :
