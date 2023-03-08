5ef6cffdd8e4699822e9992c36a7703f09c31985
jharrisGCD <jharris@gcdtech.com>
2023-03-08 16:03:15 +0000

    Displaying live camera feed

--- app/cuber-electron/public/main.js
+++ app/cuber-electron/public/main.js
@@ -15,13 +15,28 @@ function createWindow() {
         webPreferences: {
             nodeIntegration: true,
             enableRemoteModule: true,
-            contextIsolation: false
+            contextIsolation: false,
+            webSecurity: false
         }
     })
 
     win.loadURL(
  'http://localhost:3000'
         )
+
+          // Get the camera stream and display it in a video element
+  navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => {
+    const video = document.createElement('video');
+    video.setAttribute('id', 'camera');
+    video.setAttribute('class', 'camera-feed');
+    video.setAttribute('autoplay', '');
+    video.srcObject = stream;
+    document.body.appendChild(video);
+  })
+  .catch(error => {
+    console.error(error);
+  });
 }
 
 app.on('ready', createWindow)
