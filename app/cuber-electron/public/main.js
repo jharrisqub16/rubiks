4b4dc039b5c2a221b3026dabf7a506fb1bc9fb9f
jharrisGCD <jharris@gcdtech.com>
2023-03-08 17:01:29 +0000

    camera set up

--- app/cuber-electron/public/main.js
+++ app/cuber-electron/public/main.js
@@ -25,6 +25,8 @@ function createWindow() {
         )
 
           // Get the camera stream and display it in a video element
+          win.webContents.on('did-finish-load', () => {
+            win.webContents.executeJavaScript(`
   navigator.mediaDevices.getUserMedia({ video: true })
   .then(stream => {
     const video = document.createElement('video');
@@ -37,6 +39,36 @@ function createWindow() {
   .catch(error => {
     console.error(error);
   });
+  `)
+  win.webContents.executeJavaScript(`
+  navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => {
+    const video = document.createElement('video2');
+    video.setAttribute('id', 'camera2');
+    video.setAttribute('class', 'camera-feed2');
+    video.setAttribute('autoplay', '');
+    video.srcObject = stream;
+    document.body.appendChild(video);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+  `)
+  win.webContents.executeJavaScript(`
+  navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => {
+    const video = document.createElement('video3');
+    video.setAttribute('id', 'camera3');
+    video.setAttribute('class', 'camera-feed3');
+    video.setAttribute('autoplay', '');
+    video.srcObject = stream;
+    document.body.appendChild(video);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+  `)
+})
 }
 
 app.on('ready', createWindow)
